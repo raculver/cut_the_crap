b24bfe193846ac08ace8cd42a7796520ff3cfa89
robert.culver <robertculver90@gmail.com>
2026-07-19 22:30:56 +0100

    adding a bit of ugly scanning. Really need ESP 32 to go further now.

--- comms/test.py
+++ comms/test.py
@@ -1,12 +1,43 @@
 import asyncio
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 
-address = "24:71:89:cc:09:05"
-MODEL_NBR_UUID = "2A24"
+MODEL_NBR_UUID = "00001525-1212-EFDE-1523-785FEABCD123"
 
-async def main(address):
-    async with BleakClient(address) as client:
+async def test():
+    devices = await BleakScanner.discover()
+    [print(device) for device in devices]
+
+async def main():
+    devices = await BleakScanner.discover()
+    device = [device for device in devices if device.name == "Test Ad"][0]
+
+    print("Device is {}".format(device.name))
+
+    async with BleakClient(device) as client:
+        print("Connected:", client.is_connected)
+    
+#        for service in client.services:
+#            print(service.uuid)
+
+        for service in client.services:
+            print(service.uuid)
+            for char in service.characteristics:
+                print("   ", char.uuid, char.properties)
+
+#        for char in service.characteristics:
+#            print("   ", char.uuid, char.properties)
+
+#        for service in client.services:
+#            for char in service.characteristics:
+#                if char.uuid.endswith("2ab4-0000-1000-8000-00805f9b34fb"):
+#                    print(char)
+#                    value = await client.read_gatt_char(char)
+#                    print(value)
+
+
+    async with BleakClient(device.address) as client:
         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
         print(f"Model Number: {model_number.decode()}")
 
-asyncio.run(main(address))
+asyncio.run(test())
+asyncio.run(main())
